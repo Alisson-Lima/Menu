a909e77e1f9133a46d1c1868139169c29c179a88
Alisson Renan <renalima.rl46@gmail.com>
2022-07-29 22:46:53 -0300

    First files

---
+++ src/Menu.jsx
@@ -0,0 +1,13 @@
+export function Menu() {
+
+  return (
+    <div className="Menu">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Works</li>
+        <li>Contact</li>
+      </ul>
+    </div> 
+  )
+}
