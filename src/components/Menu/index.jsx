8d20239816274df375e6c6c3a042e06df2091556
Alisson Renan <renalima.rl46@gmail.com>
2022-08-01 20:46:25 -0300

    Changing focus for a new dinamic and animated focus

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,21 +1,33 @@
 import {ItemList} from "../ItemList/"
+import { useState } from "react"
 import "./style.css"
 export function Menu(){
+    const [focusW, setFocusW] = useState()
     function verify(e){
+        const event = e
+
+        // Mudar largura do foco
+        setFocusW((e.target.offsetWidth / 100) * 90 +"px")
+
+
+        // Mudar foco do link
         let focus = document.querySelector('.focus')
         if(focus == null){
-            e.target.classList.add('focus')
+            event.target.classList.add('focus')
         }else{
             focus.classList.remove('focus')
-            e.target.classList.add('focus')
+            event.target.classList.add('focus')
         }
     }
     return (
         <ul>
-            <ItemList inicialFocus="focus" content="Home" func={(e) => verify(e)}/>
-            <ItemList content="About" func={(e) => verify(e)}/>
-            <ItemList content="Works" func={(e) => verify(e)}/>
-            <ItemList content="Contact" func={(e) => verify(e)}/>
+            <div className="links">
+                <ItemList inicialFocus="focus" content="Home" func={(e) => verify(e)}/>
+                <ItemList content="About" func={(e) => verify(e)}/>
+                <ItemList content="Works" func={(e) => verify(e)}/>
+                <ItemList content="Contact" func={(e) => verify(e)}/>
+            </div>
+            <div className="focus-bar" style={{width: focusW}}></div>
         </ul>
     )
 }
